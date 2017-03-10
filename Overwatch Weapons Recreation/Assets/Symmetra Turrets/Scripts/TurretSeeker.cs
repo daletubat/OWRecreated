ecfc154a3ce274bc09fb583cbeb611659608aa18
Dale Tubat <daletubat@yahoo.com>
2017-03-10 00:54:11 -0800

    added symm turrets

---
+++ Overwatch Weapons Recreation/Assets/Symmetra Turrets/Scripts/TurretSeeker.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretSeeker : MonoBehaviour {
+    //Public Settings
+    public int seekRadius;
+    public float damage;
+
+    //Local Data
+    Collider[] objectsInRadius;
+    float lowDist;
+    
+
+	void Update () {
+        objectsInRadius = Physics.OverlapSphere(transform.position, seekRadius);
+        focusTarget();
+	}
+    void focusTarget()
+    {
+        lowDist = seekRadius;
+
+        GameObject target = null;
+        foreach (Collider collider in objectsInRadius)
+        {
+            if (collider.gameObject.GetComponent<PlayerHealth>())
+            {
+                float dist = Vector3.Distance(collider.gameObject.transform.position, transform.position);
+
+                if (dist < lowDist)
+                {
+                    lowDist = dist;
+                    target = collider.gameObject;
+                }
+            }
+        }
+        if (target)
+        {
+            target.GetComponent<PlayerHealth>().decreaseHealth(damage);
+
+            LineRenderer rend = GetComponent<LineRenderer>();
+            rend.SetPosition(0, transform.position);
+            rend.SetPosition(1, target.transform.position);
+        }
+
+    }
+}
